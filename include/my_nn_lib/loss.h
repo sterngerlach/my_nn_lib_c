9603adf313fa0ff1c55a87f2068c06050d823e6b
Stern Gerlach <std.experimental.optional@gmail.com>
2023-03-12 21:26:33 +0900

    Refactor loss.h and loss.c

--- include/my_nn_lib/loss.h
+++ include/my_nn_lib/loss.h
@@ -10,21 +10,54 @@ extern "C" {
 
 #include "my_nn_lib/tensor.h"
 
-// Forward operation for the cross entropy loss
+// Outputs for the cross-entropy loss
+typedef struct
+{
+  // Softmax of size (B, D)
+  FloatTensor* y_;
+  // Output loss
+  float        loss_;
+  // Gradient for the input of size (B, D) (used for training)
+  FloatTensor* dx_;
+} CrossEntropyLossOutputs;
+
+// Initialize the outputs for the cross-entropy loss
+void CrossEntropyLossOutputsInitialize(CrossEntropyLossOutputs* outputs,
+                                       const bool inference_only);
+
+// Free the outputs for the cross-entropy loss
+void CrossEntropyLossOutputsFree(CrossEntropyLossOutputs* outputs);
+
+// Forward operation for the cross-entropy loss
+// `x` should be a float tensor of size (B, D)
+// `target` should be a int tensor of size (B)
+// The returned float tensor `outputs->y_` is of size (B, D)
+void CrossEntropyLossForward(const FloatTensor* x,
+                             const IntTensor* target,
+                             CrossEntropyLossOutputs* outputs);
+
+// Backward operation for the cross-entropy loss
+// `target` should be a int tensor of size (B)
+// `outputs->y_` should be a float tensor of size (B, D)
+// The returned float tensor `outputs->dx_` is of size (B, D)
+void CrossEntropyLossBackward(const IntTensor* target,
+                              CrossEntropyLossOutputs* outputs);
+
+// Forward operation for the cross-entropy loss
 // `x` should be a float tensor of size (B, D)
 // `target` should be a int tensor of size (B)
 // The returned float tensor `y` is of size (B, D)
-float CrossEntropyLossForward(const FloatTensor* x,
-                              const IntTensor* target,
-                              FloatTensor* y);
+float CrossEntropyLossForwardF(const FloatTensor* x,
+                               const IntTensor* target,
+                               FloatTensor* y);
 
-// Backward operation for the cross entropy loss
+// Backward operation for the cross-entropy loss
 // `y` should be a float tensor of size (B, D)
 // `target` should be a int tensor of size (B)
 // The returned float tensor `dx` is of size (B, D)
-void CrossEntropyLossBackward(const FloatTensor* y,
-                              const IntTensor* target,
-                              FloatTensor* dx);
+void CrossEntropyLossBackwardF(const FloatTensor* y,
+                               const IntTensor* target,
+                               FloatTensor* dx);
 
 #ifdef __cplusplus
 }
--- src/my_nn_lib/loss.c
+++ src/my_nn_lib/loss.c
@@ -8,13 +8,59 @@
 
 #include <math.h>
 
-// Forward operation for the cross entropy loss
+// Initialize the outputs for the cross-entropy loss
+void CrossEntropyLossOutputsInitialize(CrossEntropyLossOutputs* outputs,
+                                       const bool inference_only)
+{
+  Assert(outputs != NULL, "`outputs` should not be NULL");
+
+  outputs->y_ = (FloatTensor*)TensorAllocate(TENSOR_TYPE_FLOAT);
+  outputs->loss_ = 0.0f;
+
+  if (!inference_only)
+    outputs->dx_ = (FloatTensor*)TensorAllocate(TENSOR_TYPE_FLOAT);
+  else
+    outputs->dx_ = NULL;
+}
+
+// Free the outputs for the cross-entropy loss
+void CrossEntropyLossOutputsFree(CrossEntropyLossOutputs* outputs)
+{
+  Assert(outputs != NULL, "`outputs` should not be NULL");
+
+  TensorFree((Tensor**)&outputs->y_);
+  TensorFree((Tensor**)&outputs->dx_);
+  outputs->loss_ = 0.0f;
+}
+
+// Forward operation for the cross-entropy loss
+// `x` should be a float tensor of size (B, D)
+// `target` should be a int tensor of size (B)
+// The returned float tensor `outputs->y_` is of size (B, D)
+void CrossEntropyLossForward(const FloatTensor* x,
+                             const IntTensor* target,
+                             CrossEntropyLossOutputs* outputs)
+{
+  outputs->loss_ = CrossEntropyLossForwardF(x, target, outputs->y_);
+}
+
+// Backward operation for the cross-entropy loss
+// `target` should be a int tensor of size (B)
+// `outputs->y_` should be a float tensor of size (B, D)
+// The returned float tensor `outputs->dx_` is of size (B, D)
+void CrossEntropyLossBackward(const IntTensor* target,
+                              CrossEntropyLossOutputs* outputs)
+{
+  CrossEntropyLossBackwardF(outputs->y_, target, outputs->dx_);
+}
+
+// Forward operation for the cross-entropy loss
 // `x` should be a float tensor of size (B, D)
 // `target` should be a int tensor of size (B)
 // The returned float tensor `y` is of size (B, D)
-float CrossEntropyLossForward(const FloatTensor* x,
-                              const IntTensor* target,
-                              FloatTensor* y)
+float CrossEntropyLossForwardF(const FloatTensor* x,
+                               const IntTensor* target,
+                               FloatTensor* y)
 {
   // The input and output tensors should not be NULL
   CheckTensor(x);
@@ -29,9 +75,9 @@ float CrossEntropyLossForward(const FloatTensor* x,
 
   // Compute the softmax from the logits
   // `y` is of size (B, D)
-  SoftmaxForward(x, y);
+  SoftmaxForwardF(x, y);
 
-  // Compute the cross entropy loss
+  // Compute the cross-entropy loss
   float loss = 0.0f;
 
   for (int b = 0; b < batch_size; ++b) {
@@ -43,13 +89,13 @@ float CrossEntropyLossForward(const FloatTensor* x,
   return -loss / (float)batch_size;
 }
 
-// Backward operation for the cross entropy loss
+// Backward operation for the cross-entropy loss
 // `y` should be a float tensor of size (B, D)
 // `target` should be a int tensor of size (B)
 // The returned float tensor `dx` is of size (B, D)
-void CrossEntropyLossBackward(const FloatTensor* y,
-                              const IntTensor* target,
-                              FloatTensor* dx)
+void CrossEntropyLossBackwardF(const FloatTensor* y,
+                               const IntTensor* target,
+                               FloatTensor* dx)
 {
   // The input and output tensors should not be NULL
   CheckTensor(y);
